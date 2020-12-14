91ccbaebdccd1ff179914697737412c47ab47e3b
Chunxian Zhang <1836891291@qq.com>
2020-12-14 15:28:25 +0800

    update 104.cpp

---
+++ 2C++/104.cpp
@@ -0,0 +1,30 @@
+#include "cpp_header.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// solution 1;
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        return root == NULL ? 0 : max(maxDepth(root -> left), maxDepth(root -> right)) + 1;
+    }
+};
