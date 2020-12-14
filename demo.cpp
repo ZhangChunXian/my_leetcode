c0671e511bfd503c5913cca53e3a942911a4e027
Chunxian Zhang <1836891291@qq.com>
2020-12-14 15:26:35 +0800

    add cpp_header.h

--- demo.cpp
+++ demo.cpp
@@ -1,60 +1,23 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-
-// addition是指针函数, 一个返回类型是指针的函数
-int* addition(int a, int b) {
-    int *sum = new int(a + b);
-    return sum;
-}
-
-int subtraction(int a, int b) {
-    return a - b;
-}
-
-int operation(int x, int y, int (*func)(int, int)) {
-    return (*func)(x, y);
-}
-
-// minus是函数指针, 指向函数的指针
-int (*minus)(int, int) = subtraction;
-
-int *m = addition(1, 2);
-int n = operation(3, *m, minus);
-
-
-
 int main() {
-    int *p = NULL;
-    p = new(nothrow) int;
-    if (!p)
-        cout << "allocation of memory failed\n";
-    else {
-        *p = 29;
-        cout << "Value of p:" << *p << endl;
+    vector<int> myvector;
+    myvector.push_back(1);
+    myvector.push_back(2);
+    myvector.push_back(3);
+    myvector.push_back(4);
+    myvector.push_back(5);
+
+    vector<int>::iterator it;
+
+    it = myvector.begin();
+    myvector.erase(it);
+
+    for (auto it = myvector.begin(); it != myvector.end(); ++it) {
+        cout << ' ' << *it;
     }
-
-    float *r = new float(75.25);
-
-    cout << "Value of r:" << *r << endl;
-
-    int n = 5;
-    int *q = new(nothrow) int[n];
-
-    if (!q)
-        cout << "allocation of memory failed\n";
-    else {
-        for (int i = 0; i < n; i++)
-            q[i] = i + 1;
-
-        cout << "Value store in block of memory: ";
-        for (int i = 0; i < n; i++)
-            cout << q[i] << " ";
-    }
-
-    delete p;
-    delete r;
-    delete[] q;
 
     return 0;
 }
