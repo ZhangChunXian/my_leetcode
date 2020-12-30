f6b0c726e2e205c695da0855c5e361c257aa175a
Chunxian Zhang <1836891291@qq.com>
2020-12-30 20:48:21 +0800

    update  283.cpp

--- 2C++/283.cpp
+++ 2C++/283.cpp
@@ -20,3 +20,22 @@ public:
         }
     }
 };
+
+/**
+ * @brief C++高赞回答, 方法双指针
+ * 
+ */
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size(), left = 0, right = 0;
+
+        while (right < n) {
+            if (nums[right]) {
+                swap(nums[left], nums.[right]);
+                left++;
+            }
+            right++;
+        }
+    }
+}
--- demo.cpp
+++ demo.cpp
@@ -1,28 +1,43 @@
-// Program below illustrates the 
-// vector::insert() function 
-  
-#include <bits/stdc++.h> 
-using namespace std; 
-  
-int main() 
-{ 
-    // initialising the vector 
-    vector<int> vec = { 10, 20, 30, 40 }; 
-  
-    // inserts 3 at front 
-    auto it = vec.insert(vec.begin(), 3); 
-    // inserts 2 at front 
-    vec.insert(it, 2); 
-  
-    int i = 2; 
-    // inserts 7 at i-th index 
-    it = vec.insert(vec.begin() + i, 7); 
+#include "cpp_header.h"
 
-    cout << "The vector elements are: "; 
-    for (auto it = vec.begin(); it != vec.end(); ++it) {
-        // *it = *it + 1;
-        cout << *it << " ";
+int main() {
+    // // get an iterator to an element in word_count
+    // auto map_it = word_count.begin();
+    // // *map_it is a reference to a pair <const string, size_t> object
+    // cout << map_it->first;      // print the key for this elemnet
+    // cout << " " << map_it->second;  // print the value of the element
+
+    // set<int> iset = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    // set<int>::iterator set_it = iset.begin();
+    // if (set_it != iset.end()) {
+    //     *
+
+    // vector<int> ivec = {2, 4, 6, 8, 2, 4, 6, 8};
+    // set<int> set2;
+    // set2.insert(ivec.begin(), ivec.end());      // set2 has four elements
+    // set2.insert({1, 3, 5, 7, 1, 3, 5, 7});
+    // map<string, size_t> word_count;
+    // string word;
+    // while (cin >> word)
+    //     ++word_count[word];
+
+    // for (const auto &w : word_count) {
+    //     cout << w.first << "occurs" << w.second
+    //     << ((w.second > 1) ? " times" : "time") << endl;
+    // }
+
+    // // four ways to add word to word_count
+    // word_count.insert({word, 1});
+    // word_count.insert(make_pair(word, 1));
+    // word_count.insert(pair<string, size_t>(word, 1));
+    // word_count.insert(map<string, size_t>::value_type(word, 1));
+
+    // more verbose way to count number of times each word occurs in the input
+    map<string, size_t> word_count;     // empty map from string to size_t
+    string word;
+
+    while (cin >> word) {
+        // inserts an element with key equal to word and value 1;
     }
-  
-    return 0; 
-} 
+}
+
