4923f72df538bff05cc46bee02947d9daed09f2b
Chunxian Zhang <1836891291@qq.com>
2020-12-31 14:50:48 +0800

    add 1.cpp

--- demo.cpp
+++ demo.cpp
@@ -1,33 +1,23 @@
-// #include <cpp_header.h>
+#include "cpp_header.h"
 
-// struct Sales_data {
-//     std::string bookNo;
-//     unsigned units_sold = 0;
-//     double revenue = 0.0;
-// }
+struct Sales_data {
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
 
-// size_t hasher(const Sales_data &sd) {
-//     return hash<string>()(sd.isbn)
-// }
+size_t hasher(const Sales_data &sd) {
+    return hash<string>()(sd.isbn());
+}
 
+bool eqOp(const Sales_data &lhs, const Sales_data &rhs) {
+    return lhs.isbn() == rhs.isbn();
+}
 
-// int main() {
-//     // // count occurrences, but the 
-//     // // words won't be in alphabetical order
-//     // unordered_map<string, size_t> word_count;
-//     // string word;
-//     // while (cin >> word) {
-//     //     ++word_count[word];     // fetch and increment the counter for word
-//     // }
+int main() {
+    using SD_multiset = unordered_multiset<Sales_data,
+                        decltype(hasher)*, decltype(eqOp)*>;
 
-//     // for (const auto &w : word_count) {    // for each element in the map
-//     //     // print the result
-//     //     cout << w.first << " occurs " << w.second
-//     //          << ((w.second > 1) ? " times" : " time") << endl;
-//     // }
-
-//     // return 0;
-    
-// }
-
-#in
+    // arguments are the bucket size and pointers to the hash function and equality operator
+    SD_multiset bookstore(42, hasher, eqOp);
+}
