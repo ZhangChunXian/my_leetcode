9d2976975330067bf77ff08da55de054c5cbbf43
Chunxian Zhang <1836891291@qq.com>
2021-01-10 20:16:30 +0800

    add 20.cpp

---
+++ 2C++/20.cpp
@@ -0,0 +1,42 @@
+#include "cpp_header.h"
+/**
+ * @brief 2021年1月10日 20:13:42 独立做出, 采用多个if else 代码不够美观, 没有用栈这种数据结构
+ * 执行用时：4 ms, 在所有 C++ 提交中击败了42.27%的用户
+ * 内存消耗：6.6 MB, 在所有 C++ 提交中击败了32.56%的用户
+ */
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.size() % 2 != 0) return false;    // 字符串元素个数为偶数是true的必要条件
+
+        string::iterator it = s.begin();
+
+        while (s.size() > 0) {
+            if (*it == '(' || *it == '[' || *it == '{') {
+                it++;
+                continue;
+            }else if (*it == ')') {
+                if (it == s.begin()) return false;
+                else if (*(it - 1) == '(') {
+                    s.erase(it - 1, it + 1);
+                    it--;
+                }else return false;
+            }else if (*it == ']') {
+                if (it == s.begin()) return false;
+                else if (*(it - 1) == '[') {
+                    s.erase(it - 1, it + 1);
+                    it--;
+                }else return false;
+            }else if (*it == '}') {
+                if (it == s.begin()) return false;
+                else if (*(it - 1) == '{') {
+                    s.erase(it - 1, it + 1);
+                    it--;
+                }else return false;
+            }
+
+            if (it == s.end() && s.size() != 0) return false;
+        }
+        return true;
+    }
+};
