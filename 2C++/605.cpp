24790281bbd21dc510d6fbe4a9b0d6eda644c043
ZhangChunXian <1836891291@qq.com>
2021-12-12 19:05:49 +0800

    update 605

--- 2C++/605.cpp
+++ 2C++/605.cpp
@@ -1,6 +1,11 @@
 #include <vector>
 using namespace std;
 
+/**
+ * @brief 不知道什么时候写的
+ * 执行用时：28 ms, 在所有 C++ 提交中击败了7.02%的用户
+ * 内存消耗：19.8 MB, 在所有 C++ 提交中击败了43.10%的用户通过测试用例：124 / 124
+ */
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
@@ -34,3 +39,36 @@ public:
         else return true;
     }
 };
+
+
+/**
+ * @brief 统一连续0的数量
+ * 执行用时：8 ms, 在所有 C++ 提交中击败了97.64%的用户
+ * 内存消耗：19.8 MB, 在所有 C++ 提交中击败了23.27%的用户
+ */
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        if(flowerbed.size() == 0) return n == 0;
+
+        int countOfZero = 1;    // 当前全0区段中连续0的数量, 刚开始预设1个0, 因为开头花坛的最左边没有花, 可以认为存在一个虚无的0
+        int canPlace = 0;       // 可以种的花的数量
+
+        for(int bed : flowerbed) {
+            if (bed == 0) { // 遇到0, 连续0的数量+1
+                countOfZero++;
+            } else {    // 遇到1, 结算上一区段连续的0区间
+                canPlace += (countOfZero - 1) / 2;
+                if (canPlace >= n) return true;
+                countOfZero = 0;
+            }
+        }
+
+        // 最后一段0还未结算
+        countOfZero++;
+        canPlace += (countOfZero - 1) / 2;
+
+        return canPlace >= n;
+    }
+};
+
